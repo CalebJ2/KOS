9b6b54a4a7e9029172ac162c89f11ca356a801bc
CalebJ2 <calebinvents@gmail.com>
2016-02-16 21:59:26 -0800

    Added trajectories addon
    Addons:TR:impactPos returns impact coordinates as vector(lat,1,lng). z=0
    if no impact detected.
    
    Haven't figured out how to return geoPosition yet.

---
+++ src/kOS/AddOns/Trajectories/Addon.cs
@@ -0,0 +1,45 @@
+﻿using kOS.Safe.Encapsulation.Suffixes;
+
+namespace kOS.AddOns.TrajectoriesAddon
+{
+    public class Addon : Suffixed.Addon
+    {
+        public Addon(SharedObjects shared) : base("TR", shared)
+        {
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes()
+        {
+            AddSuffix("IMPACTPOS", new Suffix<kOS.Suffixed.Vector>(GetImpactPos, "Get impact position. vector(lat,1,lng) z=0 if no pos"));
+        }
+
+        private kOS.Suffixed.Vector GetImpactPos()
+        {
+            var ship = FlightGlobals.ActiveVessel;
+            var body = ship.orbit.referenceBody;
+            Trajectories.Trajectory myTrajectory = Trajectories.Trajectory.fetch;
+            myTrajectory.Update();
+            foreach (var patch in myTrajectory.patches)
+            {
+                if (patch.impactPosition.HasValue)
+                {
+                    var worldImpactPos = patch.impactPosition.Value + body.position;
+                    var lat = body.GetLatitude(worldImpactPos);
+                    var lng = body.GetLongitude(worldImpactPos);
+                    while (lng < -180)
+                        lng += 360;
+                    while (lng > 180)
+                        lng -= 360;
+                    return new kOS.Suffixed.Vector(lat, 1, lng);
+                }
+            }
+            return new kOS.Suffixed.Vector(0, 0, 0);
+        }
+        public override bool Available()
+        {
+            return true;
+        }
+
+    }
+}
