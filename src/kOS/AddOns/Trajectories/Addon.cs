101ecfe97ba4436325d72aac5dbb923e688626b9
CalebJ2 <calebinvents@gmail.com>
2016-04-08 12:33:47 -0700

    Fixed available()

--- src/kOS/AddOns/Trajectories/Addon.cs
+++ src/kOS/AddOns/Trajectories/Addon.cs
@@ -1,4 +1,8 @@
-﻿using kOS.Safe.Encapsulation.Suffixes;
+﻿using System.Reflection;
+using kOS.Safe.Encapsulation.Suffixes;
+using System.Linq;
+using UnityEngine;
+using System;
 
 namespace kOS.AddOns.TrajectoriesAddon
 {
@@ -11,13 +15,24 @@ namespace kOS.AddOns.TrajectoriesAddon
 
         private void InitializeSuffixes()
         {
-            AddSuffix("IMPACTPOS", new Suffix<kOS.Suffixed.Vector>(GetImpactPos, "Get impact position. vector(lat,1,lng) z=0 if no pos"));
+            AddSuffix("IMPACTPOS", new Suffix<kOS.Suffixed.Vector>(GetImpactPos, "Get impact position. Returns vector(lat,1,lng) z=0 if position isn't available"));
         }
 
         private kOS.Suffixed.Vector GetImpactPos()
         {
+            if (Available() == false)
+            {
+                return new kOS.Suffixed.Vector(0, 0, 0);
+            } else
+            {
+                return TrajectoryImpactPos();
+            }
+        }
+        private kOS.Suffixed.Vector TrajectoryImpactPos()
+        {
             var ship = FlightGlobals.ActiveVessel;
             var body = ship.orbit.referenceBody;
+
             Trajectories.Trajectory myTrajectory = Trajectories.Trajectory.fetch;
             myTrajectory.Update();
             foreach (var patch in myTrajectory.patches)
@@ -38,7 +53,28 @@ namespace kOS.AddOns.TrajectoriesAddon
         }
         public override bool Available()
         {
-            return true;
+            Type TrType = null;//Type.GetType("Trajectories.Trajectory");
+            TrType = AssemblyLoader.loadedAssemblies
+                .Select(a => a.assembly.GetExportedTypes())
+                .SelectMany(t => t)
+                .FirstOrDefault(t => t.FullName == "Trajectories.Trajectory");
+            //Debug.Log("Trajectories.Trajectory Type: " + TrType + ", null?: " + (TrType == null));
+            if (TrType == null) return false;
+
+            MethodInfo myMethod = TrType.GetMethod("TrajectoriesInstalled");
+            //Debug.Log("TrajectoriesInstalled method info: " + myMethod + ", null?: " + (myMethod == null));
+            if (myMethod == null) return false;
+
+            object myTrajectory = Activator.CreateInstance(TrType);
+            object value = myMethod.Invoke(myTrajectory, new object[]{});
+            //Debug.Log("Instance value: " + value + ", null?: " + (value == null));
+
+            if ((bool)value == true) {
+                return true;
+            } else
+            {
+                return false;
+            }
         }
 
     }
